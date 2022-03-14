aa5c0350e906faacd9b3a6773b6f15beb36424ab
john_dev <johnhaimez314@gmail.com>
2022-03-14 14:55:52 +0100

    init creation formulaire de connexion

--- woo_widget_connexion/lib/woo_widget_connexion.dart
+++ woo_widget_connexion/lib/woo_widget_connexion.dart
@@ -1,12 +1,67 @@
 library woo_widget_connexion;
 
 import 'package:flutter/material.dart';
+import 'package:woo_widget_input/woo_widget_input.dart';
 
 class InputConnexion extends StatefulWidget {
-  String? messageValidateEmail = 'veuillez entrer une bonne adresse mail';
-  String? labelText;
+  String? Function(String?)? emailvalidator;
+  dynamic Function(dynamic)? emailonChange;
+  String? emaillabelText;
+  String? emailinitialValue;
+  String? emailmessageValidate;
+  Widget? emaillabel;
+  bool emailobscureText;
+  bool emailpassword = false;
+  bool emailemail = false;
+  Widget? emailprefixIcon;
+  Widget? emailsufixIcon;
+  dynamic Function()? emailfuncSufixIcon;
+  EdgeInsetsGeometry? emailmargin;
+  EdgeInsetsGeometry? emailpadding;
+  String? Function(String?)? pwsvalidator;
+  dynamic Function(dynamic)? pwsonChange;
+  String? pwslabelText;
+  String? pwsinitialValue;
+  String? pwsmessageValidate;
+  Widget? pwslabel;
+  bool pwsobscureText;
+  bool pwspassword = false;
+  bool pwsemail = false;
+  Widget? pwsprefixIcon;
+  Widget? pwssufixIcon;
+  dynamic Function()? pwsfuncSufixIcon;
+  EdgeInsetsGeometry? pwsmargin;
+  EdgeInsetsGeometry? pwspadding;
 
-  InputConnexion({Key? key, this.messageValidateEmail, this.labelText}) : super(key: key);
+  InputConnexion({
+    Key? key,
+    this.emailmargin,
+    this.emailpadding,
+    this.emailvalidator,
+    this.emailonChange,
+    this.emaillabelText,
+    this.emailinitialValue,
+    this.emaillabel,
+    this.emailobscureText = false,
+    this.emailprefixIcon,
+    this.emailsufixIcon,
+    this.emailfuncSufixIcon,
+    this.emailpassword = false,
+    this.emailmessageValidate,
+    this.pwsmargin,
+    this.pwspadding,
+    this.pwsvalidator,
+    this.pwsonChange,
+    this.pwslabelText,
+    this.pwsinitialValue,
+    this.pwslabel,
+    this.pwsobscureText = false,
+    this.pwsprefixIcon,
+    this.pwssufixIcon,
+    this.pwsfuncSufixIcon,
+    this.pwspassword = false,
+    this.pwsmessageValidate,
+  }) : super(key: key);
 
   @override
   State<InputConnexion> createState() => _InputConnexionState();
@@ -14,12 +69,18 @@ class InputConnexion extends StatefulWidget {
 
 class _InputConnexionState extends State<InputConnexion> {
   final RegExp emailRegex = RegExp(r"[a-z0-9\._-]+@[a-z0-9\._-]+\.[a-z]+");
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  /// valider le contenue du input email
   String? validateEmail(String? value) {
-    value!.isEmpty || !emailRegex.hasMatch(value)
-        ? widget.messageValidateEmail
+    return value!.isEmpty || !emailRegex.hasMatch(value)
+        ? widget.emailmessageValidate
         : null;
-    return null;
+  }
+
+  /// valider le contenue du input password
+  String? validatePssword(String? value) {
+    return value!.length < 6 ? '6 caractères mini' : null;
   }
 
   @override
@@ -28,41 +89,18 @@ class _InputConnexionState extends State<InputConnexion> {
       key: _formKey,
       child: Column(
         children: [
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: const InputDecoration().copyWith(
-              labelText: widget.labelText
-            ),
+          InputCustom.email(
+            labelText: widget.emaillabelText,
             validator: (value) => validateEmail(value),
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            onChanged: (value) => setState(() => _password = value),
-            validator: (value) =>
-                value!.length < 6 ? '6 caractères mini' : null,
-            obscureText: _isSecret,
-            decoration: InputDecoration(
-              suffixIcon: InkWell(
-                onTap: () => setState(() => _isSecret = !_isSecret),
-                child: Icon(
-                  !_isSecret ? Icons.visibility : Icons.visibility_off,
-                ),
-              ),
-              labelText: 'Mot de passe',
-              border: const OutlineInputBorder(),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
+          InputCustom.password(
+            labelText: widget.pwslabelText,
+            validator: (value) => validatePssword(value),
           ),
           ElevatedButton(
-            onPressed: (!emailRegex.hasMatch(_email) || _password.length < 6)
-                ? null
-                : () {},
+            onPressed: () => {
+              print(_formKey),
+            },
             child: const Text('Valider'),
           ),
         ],
